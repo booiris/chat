0f99ca85a1a2a99ab5a2d3c04cc472fc1b1a246e
booiris <booiris02@gmail.com>
2022-12-09 21:04:56 +0800

    add client

--- src-tauri/src/consts/mod.rs
+++ src-tauri/src/consts/mod.rs
@@ -1,2 +1,2 @@
-pub const IP: &str = "127.0.0.1";
+pub const IP: &str = "192.168.31.254";
 pub const SERVER_PORT: &str = "12345";
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,18 +3,10 @@
     windows_subsystem = "windows"
 )]
 
+use app::tcp::client::send_msg;
+
 use app::tcp::server::server;
 use tauri::{Manager, Window};
-
-#[tauri::command]
-fn command(text: String) -> String {
-    format!("invoke: {}", text)
-}
-
-#[tauri::command]
-fn hello() {
-    println!("hello world");
-}
 
 static mut FLAG: bool = false;
 
@@ -43,13 +35,18 @@ async fn main() {
 
             let main_window = app.get_window("main").unwrap();
 
-            main_window.listen("front-event", |event| {
-                println!("got window event-name with payload {:?}", event.payload());
+            main_window.listen("send-msg", |event| {
+                tokio::spawn(async move {
+                    match send_msg(event.payload()).await {
+                        Ok(()) => (),
+                        Err(err) => eprintln!("error: {}", err),
+                    }
+                });
             });
 
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![command, hello, init_process])
+        .invoke_handler(tauri::generate_handler![init_process])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
