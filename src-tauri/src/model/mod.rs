270b37e05660287fa700abd8e6e7769d7033bcec
booiris <954520770@qq.com>
2022-12-12 12:24:34 +0800

    u

--- src-tauri/src/model/mod.rs
+++ src-tauri/src/model/mod.rs
@@ -1,4 +1,17 @@
 #[derive(Clone, serde::Serialize, serde::Deserialize)]
 pub struct Payload {
     pub message: String,
+    pub time: time
+}
+
+#[derive(Clone, serde::Serialize, serde::Deserialize, Debug)]
+pub struct IdStruct {
+    pub id: i64,
+    pub ip: String,
+    pub port: String,
+}
+
+#[derive(Clone, serde::Serialize, serde::Deserialize, Debug)]
+pub struct DbData {
+    pub id_data: IdStruct,
 }
