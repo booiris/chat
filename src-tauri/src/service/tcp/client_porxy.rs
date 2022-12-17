55cbe039426e736011f2aa12ce6c24ac24ad1f06
booiris <60592744+booiris@users.noreply.github.com>
2022-12-17 16:26:31 +0800

    Revert "u"
    This reverts commit 90491f4e3596e44071c0305cca8f6d0f60bf0b3c.

--- src-tauri/src/service/tcp/client_porxy.rs
+++ src-tauri/src/service/tcp/client_porxy.rs
@@ -54,3 +54,41 @@ impl ClientProxy {
         Ok(())
     }
 }
+
+// async fn send_msg<'a>(
+//     user_id: i64,
+//     pay_load: Option<&str>,
+//     db: Db,
+// ) -> Result<(), Box<dyn Error + Send + Sync>> {
+//     let a = DB.lock().unwrap();
+//     while DB.lock().await.get(&user_id).is_none() {
+//         let mut stream = TcpStream::connect(format!("{}:{}", "192.168.31.171", "8086")).await?;
+//         let id_struct = IdStruct {
+//             id: user_id,
+//             ip: "127.0.0.1".into(),
+//             port: "8086".into(),
+//         };
+//         let clien_req = ClientReq {
+//             aim_user: user_id,
+//             client: id_struct,
+//         };
+//         let data = serde_json::to_vec(&clien_req).unwrap();
+//         stream.write_all(&data).await?;
+//         stream.shutdown().await?;
+//         let mut buffer = Vec::new();
+//         stream.read_to_end(&mut buffer).await?;
+//         if let Some(resp) = serde_json::from_slice::<ClientResp>(&buffer)?.aim_user {
+//             db.lock().await.insert(user_id, DbData { id_data: resp });
+//         }
+//         sleep(time::Duration::from_millis(100));
+//     }
+//     info!("{:?}", pay_load);
+//     let db_data = db.lock().await.get(&user_id).unwrap().clone();
+//     let ip = &db_data.id_data.ip;
+//     let port = &db_data.id_data.port;
+//     let mut stream = TcpStream::connect(format!("{}:{}", ip, port)).await?;
+//     stream
+//         .write_all(pay_load.unwrap_or_default().as_bytes())
+//         .await?;
+//     Ok(())
+// }
